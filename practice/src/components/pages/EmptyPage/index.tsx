864e26d91bffa4f33f6c7e0bf5c075880e9f56ee
Margarita <Margarita>
2023-03-29 22:30:44 +0300

    feat: add not found category page

---
+++ practice/src/components/pages/EmptyPage/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './style.module.scss';
+
+export const EmptyPage = () => {
+  return <div className={`page ${classes.page}`}>No images found</div>;
+};
