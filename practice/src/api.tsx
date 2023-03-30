46771a573ab40689bac9ef0914f61b443036477c
Margarita <Margarita>
2023-03-30 15:31:45 +0300

    feat: update api

--- practice/src/api.tsx
+++ practice/src/api.tsx
@@ -3,16 +3,20 @@ import axios from 'axios';
 export const API = 'https://api.pexels.com/v1/';
 export const API_KEY = '563492ad6f917000010000014640aabb4e9d420cbe1c0df7daf4c2bf';
 
-export const getPhotos = async () => {
-  const res = await axios.get(`${API}curated`, {
+export const getPhotos = async (page = 1) => {
+  const res = await axios.get(`${API}curated?page=${page}`, {
     headers: { Accept: 'application/json', Authorization: API_KEY },
   });
   return await res.data.photos;
 };
 
-export const getPhotosById = async (id: string, params = { sizes: '', orientations: '' }) => {
+export const getPhotosById = async (
+  id: string,
+  params = { sizes: '', orientations: '' },
+  page = 1
+) => {
   const res = await axios.get(
-    `${API}search?query=${id}
+    `${API}search?query=${id}&page=${page}
 		${params.orientations && `&orientation=${params.orientations.toLowerCase()}`}
 		${params.sizes && `&size=${params.sizes.toLowerCase()}`}`,
     {
@@ -20,4 +24,11 @@ export const getPhotosById = async (id: string, params = { sizes: '', orientatio
     }
   );
   return await res.data.photos;
+};
+
+export const getImageFile = async (url: string) => {
+  const res = await axios.get(url, {
+    responseType: 'blob',
+  });
+  return await res.data;
 };
